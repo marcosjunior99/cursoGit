8166b671c5bf5e94a7ac07e21657811a22a34c4c
Marcos Junior <juniorvoip@hotmail.com>
2017-01-30 16:30:35 -0200

    Iniciando Projeto com index.php e estilo.css

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<title>Curso de Git</title>
+</head>
+<body>
+	<h1>Página do Curso</h1>
+	<section class="container header">
+		<div class="content">
+			<h1>Cabeçalho</h1>
+		</div>
+	</section>
+	<section class="container">
+		<div class="content">
+			<h1>Corpo do Site</h1>
+		</div>
+	</section>
+	<section class="container">
+		<div class="content footer">
+			<h1>Rodapé</h1>
+		</div>
+	</section>
+</body>
+</html>
