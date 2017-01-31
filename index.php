848298c8fda3edc1f7371a0dbe57577ed6cc8a36
Marcos Junior <juniorvoip@hotmail.com>
2017-01-31 15:12:07 -0200

    Inclusão do texto paragrafo

--- index.php
+++ index.php
@@ -14,12 +14,12 @@
 	<section class="container">
 		<div class="content">
 			<h1>Corpo do Site</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi cumque consequatur, tempore molestiae illo quo in voluptate odio dolorum quam unde eum consectetur, corporis eligendi ducimus. Provident voluptate quos ipsum, culpa doloremque amet mollitia vitae distinctio sit perspiciatis, a repellat vel quae, atque ipsa sed ea recusandae tempora illum tempore rem. Ut repellat harum dicta quae placeat sed dolore saepe quasi quod explicabo temporibus illum obcaecati cupiditate nam totam mollitia officia veritatis maiores neque error, blanditiis. Distinctio dignissimos itaque perferendis autem quasi iure consequatur, placeat doloribus, molestiae tempore suscipit quisquam nihil nostrum id sed architecto aliquid porro pariatur necessitatibus asperiores?</p>
 		</div>
 	</section>
 	<section class="container">
 		<div class="content footer">
 			<h1>Rodapé</h1>
-			<h2>Vinicius</h2>
 		</div>
 	</section>
 </body>
